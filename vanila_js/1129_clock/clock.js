09734d77b7cb7eb8419e38aa620bd03a05e610f7
noder7 <historikaper@gmail.com>
2019-11-30 01:05:59 +0900

    practice methods of classList

---
+++ vanila_js/1129_clock/clock.js
@@ -0,0 +1,33 @@
+const title = document.querySelector('#title');
+const CLICKED_CLASS = 'clicked';
+let date, hour, min, sec;
+
+function getTime(){
+    date = new Date();
+    hour = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    title.innerHTML = `${hour < 10 ? `0${hour}` : hour}:${
+        min < 10 ? `0${min}` : min
+    }:${sec < 10 ? `0${sec}` : sec}`;
+};
+
+function HandleClick() {
+    const currentClass = title.className;
+    title.classList.toggle(CLICKED_CLASS);
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // }else{
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+title.addEventListener('click', HandleClick);
+init();
+
